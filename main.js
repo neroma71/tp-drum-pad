4232c4313ada07038de9e3d4f23f5439e314b71c
neroma71 <neroma71@gmail.com>
2023-06-26 13:11:11 +0200

    end oe drum pad

--- main.js
+++ main.js
@@ -1,2 +1,58 @@
-let pads = document.querySelector('.pad div');
-let sounds = document.querySelector('.sounds');
+
+let pads = document.querySelectorAll('.pad div');
+let sounds = document.querySelectorAll('.sounds');
+
+pads.forEach((pad, index)=>{
+    pad.addEventListener('click', function(){
+       sounds[index].currentTime = 0;
+       sounds[index].play(); 
+    });
+    });
+window.addEventListener('keydown', (e)=>{
+    soundplay(e);
+});
+function soundplay(e){
+    let datakey;
+switch(e.key){
+    case "a":
+        datakey="65";
+       break;
+    case "z":
+        datakey="90";
+       break;
+    case "e":
+        datakey="69";
+          break;
+   case "r":
+         datakey="82";
+          break;
+   case "t":
+        datakey="83";
+           break;
+   case "y":
+        datakey="85";
+           break;
+   case "u":
+        datakey="73";
+           break;
+   case "i":
+        datakey="79"; 
+           break;
+   case "o":
+        datakey="80"; 
+           break;
+       }
+       let sound = document.querySelector('audio[data-key="' + datakey + '"]');
+       sound.currentTime = 0;
+       sound.play();
+    
+       let div = document.querySelector('div[data-key="' + datakey + '"]');
+
+       function remove(){
+        div.classList.remove('playing');
+       }
+        div.classList.add('playing');
+     setTimeout(remove, 100);
+    }
+
+    
