b54cfbf14a3709eef700f43b46c2b207dd96da16
neroma71 <neroma71@gmail.com>
2023-06-27 13:28:55 +0200

    corrigé

--- main2.js
+++ main2.js
@@ -1,8 +1,3 @@
-let keys = document.querySelectorAll(".key");
-let audio;
-let key;
-let pads = document.querySelectorAll('.pad div');
-let sounds = document.querySelectorAll('.sounds');
 
 pads.forEach((pad, index)=>{
     pad.addEventListener('click', function(){
@@ -11,72 +6,88 @@ pads.forEach((pad, index)=>{
     });
     });
 
-window.addEventListener("keydown", playSound);
-
-keys.forEach(function(key){
-    key.addEventListener("transitionend", removeTransition);
-});
-
-function playSound(event){
-    audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-
-    if(!audio) return;
-
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add("playing");
-}
-
-function removeTransition(event){
-    event.target.classList.remove("playing");
-}
-
-async function beatBox() {
-    function simulateKey(keyCode) {
-      let event = new KeyboardEvent("keydown", {
-        keyCode: keyCode,
-      });
-      window.dispatchEvent(event);
-    }
-  
-    function playBeat(keyCode, time) {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve(simulateKey(keyCode));
-        }, time);
-      });
-    }
-  
-  
-    while(loop){
-      await playBeat(65, 100);
-      await playBeat(90, 200);
-      await playBeat(88, 250);
+    let keys = document.querySelectorAll(".key");
+    let audio;
+    let key;
     
-      await playBeat(81, 300);
-      await playBeat(68, 400);
-      await playBeat(67, 450);
+    window.addEventListener("keydown", playSound);
+    // window.addEventListener("keydown", (event) => {
+    //  console.log(event);
+    // });
     
-      await playBeat(87, 500);
-      await playBeat(83, 550);
-      await playBeat(69, 600);
+    
+    keys.forEach(function (key) {
+      key.addEventListener("transitionend", removeTransition);
+    });
+    
+    function playSound(event) {
+      // console.log(event);
+      audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+      key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    
+      if (!audio) return;
+    
+      audio.currentTime = 0;
+      audio.play();
+      key.classList.add("playing");
     }
     
-  }
-  
-  let loop;
-  document.addEventListener("keydown", (event) => {
-     loop = true;
-     // Vérifie si la touche appuyée est la touche G (keyCode 71)
-    if (event.keyCode === 71) {    
-        beatBox(); // Lance la séquence de "beats"
+    function removeTransition(event) {
+      event.target.classList.remove("playing");
     }
-  });
-  
-  document.addEventListener("keydown", (event) => {
-    // Vérifie si la touche appuyée est la touche H (keyCode 72)
-    if (event.keyCode === 72){
-      loop = false;
-    }    
-  });
+    
+    
+    
+    // /*##########################################*/
+    
+    async function beatBox() {
+      function simulateKey(keyCode) {
+        let event = new KeyboardEvent("keydown", {
+          keyCode: keyCode,
+        });
+        window.dispatchEvent(event);
+      }
+    
+      function playBeat(keyCode, time) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(simulateKey(keyCode));
+          }, time);
+        });
+      }
+    
+    
+      while(loop){
+        await playBeat(65, 100);
+        await playBeat(90, 200);
+        await playBeat(88, 250);
+      
+        await playBeat(81, 300);
+        await playBeat(68, 400);
+        await playBeat(67, 450);
+      
+        await playBeat(87, 500);
+        await playBeat(83, 550);
+        await playBeat(69, 600);
+      }
+      
+    }
+    
+    let loop;
+    document.addEventListener("keydown", (event) => {
+       loop = true;
+       // Vérifie si la touche appuyée est la touche G (keyCode 71)
+      if (event.keyCode === 71) {    
+          beatBox(); // Lance la séquence de "beats"
+      }
+    });
+    
+    document.addEventListener("keydown", (event) => {
+      // Vérifie si la touche appuyée est la touche H (keyCode 72)
+      if (event.keyCode === 72){
+        loop = false;
+      }    
+    });
+    
+    
+    
